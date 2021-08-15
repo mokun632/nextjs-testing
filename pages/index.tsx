b69c34eea516f490d67bafb1687cc82a1f1b1663
motohiro yamagishi <motohiro993@gmail.com>
2021-08-15 18:32:00 +0900

    !

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 const Home: React.VFC = () => {
   return (
     <Layout title="Home">
-      <p className="text-4xl">Welcome to Nextjs</p>
+      <p className="text-4xl">Welcome to Nextjs!</p>
     </Layout>
   )
 }
